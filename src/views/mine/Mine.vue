0ed9b5182d4513cbf1953bc5ed13aa02d615a701
heshuyi <15302006236@163.com>
2022-04-03 00:18:44 +0800

    123

--- src/views/mine/Mine.vue
+++ src/views/mine/Mine.vue
@@ -6,75 +6,94 @@
   </div>
 </template>
 <script>
-import { url as urlqing } from "../../js/url";
+// import { url as urlqing } from "../../js/url";
+let socket = null;
 export default {
-  socket: 1,
   name: "Mine",
   data() {
     return {
       myData: null,
-      socket: null,
+      // socket: null,
       log: null,
     };
   },
+  beforeCreate() {
+    console.log(socket);
+  },
   created() {
     // this.initWebSocket();
-    this.$http.get(urlqing + "/api11", { params: { token: 1 } }).then((res) => {
-      console.log(res);
-    });
+    // this.$http.get(urlqing + "/api11", { params: { token: 1 } }).then((res) => {
+    //   console.log(res);
+    // });
+    socket = new WebSocket("ws://localhost:9999");
+    //当连接成功之后就会执行回调函数
+    socket.onopen = function () {
+      console.log("客户端连接成功");
+      //再向服务器发送一个消息
+      socket.send("didi"); //客户端发的消息内容 为hello
+    };
+    //绑定事件是用加属性的方式
+    socket.onmessage = function (event) {
+      console.log("收到服务器端的响应", event);
+    };
   },
   monted() {
     //绑定事件是用加属性的方式
   },
   destroyed() {
-    // socket.close()
+    console.log(1111);
+    socket.onclose = function (evt) {
+      console.log("Connection closed."+evt);
+    };
+    socket.close()
     console.log(1);
-    // this.websocket.close();
+    
+    // socket = null
   },
   methods: {
     tuichu() {
       window.sessionStorage.clear();
       this.$router.push("/login");
     },
-    initWebSocket() {
-      const wsurl = "ws://localhost:9999"; //"wss://www.kaycee.com/wss/test"; //ws 相当于http 而wss 相当于https
-      this.websocket = new WebSocket(wsurl); //实例对象
-      this.websocket.onmessage = this.websocketonmessage;
-      this.websocket.onopen = this.websocketonopen;
-      this.websocket.onerror = this.websocketonerror;
-      this.websocket.onclose = this.websocketclose;
-    },
-    //连接建立
-    websocketonopen() {
-      console.log("连接建立成功");
-      this.websocketsend();
-      this.timer = setInterval(() => {
-        this.doSend();
-      }, 3600 * 1000);
-    },
+    // initWebSocket() {
+    //   const wsurl = "ws://localhost:9999"; //"wss://www.kaycee.com/wss/test"; //ws 相当于http 而wss 相当于https
+    //   this.websocket = new WebSocket(wsurl); //实例对象
+    //   this.websocket.onmessage = this.websocketonmessage;
+    //   this.websocket.onopen = this.websocketonopen;
+    //   this.websocket.onerror = this.websocketonerror;
+    //   // this.websocket.onclose = this.websocketclose;
+    // },
+    // //连接建立
+    // websocketonopen() {
+    //   console.log("连接建立成功");
+    //   this.websocketsend();
+    //   this.timer = setInterval(() => {
+    //     this.doSend();
+    //   }, 3600 * 1000);
+    // },
 
-    //websocket心跳  防连接超时  WebSocket规定在一定时间内没有数据交互，就会自动断开
-    doSend() {
-      this.websocket.send("didi");
-    },
+    // //websocket心跳  防连接超时  WebSocket规定在一定时间内没有数据交互，就会自动断开
+    // doSend() {
+    //   this.websocket.send("didi");
+    // },
 
-    //发送数据
-    websocketsend() {
-      this.websocket.send("didi1"); //发送数据，传什么数据由实际需求决定
-    },
-    //数据接收
-    websocketonmessage(e) {
-      console.log("接收后端返回数据", e.data);
-    },
+    // //发送数据
+    // websocketsend() {
+    //   this.websocket.send("didi1"); //发送数据，传什么数据由实际需求决定
+    // },
+    // //数据接收
+    // websocketonmessage(e) {
+    //   console.log("接收后端返回数据", e.data);
+    // },
 
-    //连接建立失败重连
-    websocketonerror() {
-      this.initWebSocket();
-    },
-    //连接关闭
-    websocketclose(e) {
-      console.log("websocket断开连接", e);
-    },
+    // //连接建立失败重连
+    // websocketonerror() {
+    //   this.initWebSocket();
+    // },
+    // //连接关闭
+    // websocketclose(e) {
+    //   console.log("websocket断开连接", e);
+    // },
   },
 };
 </script>
